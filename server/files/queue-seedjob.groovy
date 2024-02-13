7fb299f944f196a8eeae51318bf6f98c709f5646
Daniel Gavrila <danielduduta@gmail.com>
2024-02-13 13:24:44 -0600

    cleanup

--- server/files/queue-seedjob.groovy
+++
@@ -1,7 +0,0 @@
-import javaposse.jobdsl.dsl.DslScriptLoader
-import javaposse.jobdsl.plugin.JenkinsJobManagement
-
-println 'Queuing seed-job at Jenkins startup ...'
-def jobManagement = new JenkinsJobManagement(System.out, [:], new File('.'))
-new DslScriptLoader(jobManagement).runScript('queue("Seed or update all pipelines")')
-
